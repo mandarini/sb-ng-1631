0b710854e7c431fd6344a1c26842a3e16a2e611c
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-06 14:44:49 +0300

    added angular lib and component

---
+++ libs/ui-components/src/lib/my-cmp/my-cmp.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngsb1631-my-cmp',
+  templateUrl: './my-cmp.component.html',
+  styleUrls: ['./my-cmp.component.css'],
+})
+export class MyCmpComponent {}
---
+++ libs/ui-components/src/lib/ui-components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyCmpComponent } from './my-cmp/my-cmp.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [MyCmpComponent],
+})
+export class UiComponentsModule {}
